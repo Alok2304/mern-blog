467fb47826015ae930e696862cf7502b629eac13
Alok Sandip Kale <alokkale2304@gmail.com>
2024-03-02 18:24:56 +0530

    Made dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Header from "./components/Header.jsx";
 import FooterCom from "./components/Footer.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 export default function App() {
 	return (
@@ -15,7 +16,9 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/about" element={<About />} />
-				<Route path="/dashboard" element={<Dashboard />} />
+				<Route element={<PrivateRoute />}>
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Route>
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/sign-in" element={<SignIn />} />
 				<Route path="/sign-up" element={<SignUp />} />
